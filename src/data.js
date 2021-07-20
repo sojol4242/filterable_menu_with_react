e164a7ccd3bad26fcd57dd916a96486bafe4cb80
Md Moniruzzaman Sojol <bsmrstueee90@gmail.com>
2021-07-20 21:24:57 +0600

    fiterable_menu

---
+++ src/data.js
@@ -0,0 +1,82 @@
+const menu = [
+  {
+    id: 1,
+    title: "Netflix-clone",
+    category: "clone(front-end)",
+
+     
+    img: "https://i.ibb.co/qMFZFLK/Screenshot-10.png",
+    desc: `I'm baby woke mlkshk wolf bitters live-edge blue bottle, hammock freegan copper mug whatever cold-pressed `,
+    liveLink: `https://netflix-clone-bf4fd.web.app/`,
+    github: `https://github.com/sojol4242/Netflix-clone`,
+  },
+  {
+    id: 2,
+    title: "Photography Agency App",
+    category: "MERN Stack",
+
+ 
+    img:
+      "https://camo.githubusercontent.com/7a14d8b464d3e356189f6f20bc87fc5b9e4e2063aaac825b95a29bacb8340aa7/68747470733a2f2f692e6962622e636f2f38636a67665a792f53637265656e73686f742d31312e706e67",
+    desc: `vaporware iPhone mumblecore selvage raw denim slow-carb leggings gochujang helvetica man braid jianbing. Marfa thundercats `,
+    liveLink: `https://photography-2021.web.app/`,
+    github: `https://github.com/sojol4242/photography`,
+  },
+  {
+    id: 3,
+    title: "Laptop-Bazar App",
+    category: "MERN Stack",
+ 
+    img:
+      "https://github.com/Porgramming-Hero-web-course/full-stact-explore/raw/main/Screenshot/Screenshot%20(12).png",
+    desc: `ombucha chillwave fanny pack 3 wolf moon street art photo booth before they sold out organic viral.`,
+    liveLink: `https://laptop-bazar-2e5ba.web.app/`,
+    github: `https://github.com/Porgramming-Hero-web-course/full-stact-explore`,
+  },
+  {
+    id: 4,
+    title: "Amazon-clone",
+    category: "clone(front-end)",
+
+   
+    img:
+      "https://d1sr9z1pdl3mb7.cloudfront.net/wp-content/uploads/2020/06/11110622/amazon-biometric-facial-recognition.jpg",
+    desc: `Shabby chic keffiyeh neutra snackwave pork belly shoreditch. Prism austin mlkshk truffaut, `,
+    liveLink: `https://netflix-clone-bf4fd.web.app/`,
+    github: `https://github.com/sojol4242/amazon-clone`,
+  },
+  {
+    id: 5,
+    title: "Hackerrank",
+    category: "Problem solving",
+    
+    img:
+      "https://www.hackerrank.com/wp-content/uploads/2018/08/hackerrank_logo.png",
+    desc: `franzen vegan pabst bicycle rights kickstarter pinterest meditation farm-to-table 90's pop-up `,
+    liveLink: `https://www.hackerrank.com/bsmrstueee90`,
+    github: `https://github.com/sojol4242/HackerRank`,
+  },
+  {
+    id: 6,
+    title: "URI",
+    category: "Problem solving",
+ 
+    img:
+      "https://dka575ofm4ao0.cloudfront.net/pages-transactional_logos/retina/9144/Rl1qxNZhT5u7Bii1tesO",
+    desc: `Portland chicharrones ethical edison bulb, palo santo craft beer chia heirloom iPhone everyday`,
+    liveLink: `https://www.urionlinejudge.com.br/judge/en/profile/483482`,
+    github: `https://github.com/sojol4242/Uri-Problem`,
+  },
+  {
+    id: 7,
+    title: "Facebook-Clone",
+    category: "clone(front-end)",
+
+ 
+    img: "https://i.ibb.co/Wx49yHw/Screenshot-11.png",
+    desc: `carry jianbing normcore freegan. Viral single-origin coffee live-edge, pork belly cloud bread iceland put a bird `,
+    liveLink: `https://facebook-clone-b9787.web.app/`,
+    github: `https://github.com/sojol4242/facebook-clone`,
+  } 
+];
+export default menu;
